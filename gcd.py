24825c0dbd3eab5959aaf335c5cbdb79985294cc
Shreyas Jaiswal <jaiswalshreyas87@gmail.com>
2025-08-25 18:11:12 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,7 +1,15 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+import math
+while True:
+    try: #to make sure user enters the values
 
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        break
+    except ValueError:
+        print("error=> wrong input, try again!!")
+        continue
 # greatest common divisor
-result = num1 + num2
 
-print("The GCD of", num1, "and", num2, "is", result)
+result = math.gcd(num1,num2)
+
+print(f"The GCD of {num1} and {num2} is {result}") #using f string
