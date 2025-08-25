9501823b043133fa163f06a5906d347e4bdfb160
Shreyas Jaiswal <jaiswalshreyas87@gmail.com>
2025-08-25 18:25:45 +0530

    Update simple_interest.py
    -added a simple error handling system
    -used f strings
    -corrected the formula for simple interest

--- simple_interest.py
+++ simple_interest.py
@@ -1,8 +1,14 @@
-principal = float(input("Enter the principal amount: "))
-rate = float(input("Enter the interest rate (in percentage (1-100)): "))
-time = float(input("Enter the number of years for the investment: "))
+while True:
+  try:
+    principal = float(input("Enter the principal amount: "))
+    rate = float(input("Enter the interest rate (in percentage (1-100)): "))
+    time = float(input("Enter the number of years for the investment: "))
+    break
+  except ValueError:
+    print("enter the correct value\n")
+    continue
 
 # Simple Interest Calculation
 interest = (principal * rate * time)/100 #correct formula
 
-print("The simple interest for an investment of", principal, "at", rate, "% for", time, "years is:", interest)
+print(f"The simple interest for an investment of {principal} at, {rate}, % for, {time}, years is:, {interest}")
